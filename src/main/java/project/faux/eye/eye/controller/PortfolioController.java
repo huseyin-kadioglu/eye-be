0106d714b1b29ff1f6e485a60d90b22c3127e534
huseyin <huseyinavnikadioglu@gmail.com>
2023-10-20 02:13:34 +0300

    adding missing annotations

--- src/main/java/project/faux/eye/eye/controller/PortfolioController.java
+++ src/main/java/project/faux/eye/eye/controller/PortfolioController.java
@@ -1,6 +1,8 @@
 package project.faux.eye.eye.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.faux.eye.eye.model.entity.Portfolio;
@@ -9,12 +11,14 @@ import project.faux.eye.eye.service.PortfolioService;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/portfolios")
 public class PortfolioController {
 
     @Autowired
     private PortfolioService portfolioService;
 
+    @GetMapping
     public List<Portfolio> getAll() {
         return portfolioService.getAllPortfolio();
     }
